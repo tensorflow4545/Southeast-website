c33a4b8545f6442ed231e4086324eb5fa3ca9a47
tensorflow4545 <anshulojha1214@gmail.com>
2024-07-15 20:51:44 +0530

    University page

---
+++ src/app/university/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Universitypage/Navbar";
+import Auth from "@/components/Universitypage/Auth";
+export default function Home() {
+  return (
+   <>
+      <Navbar/>
+      <Auth/>
+   </>
+  );
+}
